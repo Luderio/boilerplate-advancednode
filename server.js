65c0f79a9a060600ec3e4846be28afaef3541535
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2021-12-23 16:22:53 +0800

    How to Put a Profile Together

--- server.js
+++ server.js
@@ -28,10 +28,7 @@ app.use(passport.session());
 
 myDB(async (client) => {
   const myDataBase = await client.db('database').collection('users');
-
-  // Be sure to change the title
   app.route('/').get((req, res) => {
-    // Change the response to render the Pug template
     res.render('pug', {
       title: 'Connected to Database',
       message: 'Please login',
@@ -44,7 +41,7 @@ myDB(async (client) => {
   });
 
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
-    res.render(process.cwd() + '/views/pug/profile');
+    res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
   });
 
   // Serialization and deserialization here...
