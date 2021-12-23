e2724c7b40e55a74560d3636519894a5c7a5f266
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2021-12-23 09:26:51 +0800

    Advanced Node and Express: Implement the Serialization of a Passport User

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
 const ObjectID = require('mongodb').ObjectID;
+const e = require('express');
 
 const app = express();
 app.set('view engine', 'pug');
@@ -25,19 +26,30 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+myDB(async (client) => {
+  const myDataBase = await client.db('database').collection('users');
 
-app.route('/').get((req, res) => {
-  res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'Please login' });
-});
+  app.route('/').get((request, response) => {
+    response.render('pug', {
+      title: "Connected to the Database",
+      message: "Please login"
+    });
+  });
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({_id: new ObjectID(id)}, (err, doc) => {
+      done(null, doc);
+    });
+  });
 
-passport.deserializeUser((id, done) => {
-  //myDataBase.findOne({_id: new ObjectID(id)}, (err, doc) => {
-    done(null, null);
-  //});
+}).catch(e => {
+  app.route('/').get((request, response) => {
+    response.render('pug', {title: e, message: 'Unable to login'});
+  });
 });
 
 app.listen(process.env.PORT || 3000, () => {
