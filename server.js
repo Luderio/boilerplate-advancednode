fd64910fc968cff915224b52dd7389123fba102e
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2021-12-23 14:46:30 +0800

    Implement the Serialization of a Passport User

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ myDB(async (client) => {
 
   app.route('/').get((req, res) => {
     res.render(process.cwd() + '/views/pug/index.pug', {
-      title: 'Connected to Database',
+      title: 'Connected to the Database',
       message: 'Please login'
     });
   });
@@ -44,17 +44,7 @@ myDB(async (client) => {
       done(null, doc);
     });
   });
-  passport.use(new LocalStrategy(
-    function(username, password, done) {
-      myDataBase.findOne({ username: username }, function (err, user) {
-        console.log('User '+ username +' attempted to log in.');
-        if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        if (password !== user.password) { return done(null, false); }
-        return done(null, user);
-      });
-    }
-  ));
+  
 }).catch((e) => {
   app.route('/').get((req, res) => {
     res.render(process.cwd() + '/views/pug/index.pug', { title: e, message: 'Unable to login' });
